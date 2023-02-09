2e54ca7b32a71e1ab159e5f4afce1ce2cac902e1
Crocodile <wwwblogger1@gmail.com>
2023-02-09 03:13:32 +0200

    add two files

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+
+
+
+namespace Lesson
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 1, b = 5;
+
+            Console.WriteLine($"a = {a}\t b = {b}");
+
+            Swap(ref a, ref b);
+
+            Console.WriteLine($"a = {a}\t b = {b}");
+        }
+
+        static void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+
+        
+    }
+}
